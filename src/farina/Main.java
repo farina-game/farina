420f07c5ed1dc76e8e39265056676d0e1b2ef496
FalseCAM <FalseCAM@googlemail.com>
2014-09-16 08:23:25 +0200

    Nifty Gui Menu
    Menu Main
    Menu Config
    Loading Screen

--- src/farina/Main.java
+++ src/farina/Main.java
@@ -1,43 +1,63 @@
 package farina;
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
-import com.jme3.math.Vector3f;
+import com.jme3.niftygui.NiftyJmeDisplay;
 import com.jme3.renderer.RenderManager;
-import com.jme3.scene.Geometry;
-import com.jme3.scene.shape.Box;
+import com.jme3.system.AppSettings;
+import de.lessvoid.nifty.Nifty;
+import farina.menu.MenuScreens;
 
 /**
- * test
- * @author normenhansen
+ *
+ *
+ * @author FalseCAM
  */
 public class Main extends SimpleApplication {
 
+    Nifty nifty;
+    MenuScreens menuScreens;
+
     public static void main(String[] args) {
+        AppSettings settings = new AppSettings(true);
+        settings.setTitle("MementoMori");
         Main app = new Main();
+        app.setSettings(settings);
+        app.setShowSettings(false);
         app.start();
+    }
+
+    public Nifty getNifty() {
+        return nifty;
     }
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
+        initNiftyGui();
+        menuScreens = new MenuScreens();
+        getStateManager().attach(menuScreens);
+    }
 
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
+    /**
+     * Initializes the nifty gui and loads xml files
+     */
+    void initNiftyGui() {
+        NiftyJmeDisplay niftyDisplay = new NiftyJmeDisplay(getAssetManager(),
+                getInputManager(), getAudioRenderer(), getGuiViewPort());
+        nifty = niftyDisplay.getNifty();
+        nifty.getSoundSystem().setMusicVolume(
+                1);
+        nifty.getSoundSystem().setSoundVolume(
+                1);
+        nifty.addXml("Interface/screen_loading.xml");
+        nifty.gotoScreen("screen_loading");
+        getGuiViewPort().addProcessor(niftyDisplay);
     }
 
     @Override
     public void simpleUpdate(float tpf) {
-        //TODO: add update code
     }
 
     @Override
     public void simpleRender(RenderManager rm) {
-        //TODO: add render code
     }
 }
